62f849b7d4b5276f3334ed67b8c03477edce6506
Florian Anderiasch <fa@art-core.org>
2014-02-17 23:06:01 +0100

    Add convert script mysql->postgres

---
+++ misc/mysql2postgres.php
@@ -0,0 +1,41 @@
+<?php
+$user = "multiplex";
+$pass = "multiplex";
+
+$db1 = new PDO("mysql:host=localhost;dbname=simplex", $user, $pass);
+$db1->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+$user = "multiplex";
+$pass = "multiplex";
+
+$db2 = new PDO("pgsql:host=localhost;port=15432;dbname=multiplex", $user, $pass);
+#################################################################################
+
+$sql = "SELECT * FROM clj ORDER BY id ASC";
+
+$q1 = $db1->query($sql);
+
+$errors = array();
+
+while ($row = $q1->fetch()) {
+    $sql = "INSERT INTO clj (id, author, itemtype, url, txt, meta, tag, created, updated) VALUES('%s','%s','%s','%s','%s','%s','%s','%s','%s');";
+    $sql = sprintf(
+        $sql,
+        $row['id'],
+        $row['author'],
+        $row['itemtype'],
+        $row['url'],
+        str_replace("'", "''", $row['txt']),
+        str_replace("'", "''", $row['meta']),
+        $row['tag'],
+        $row['created'],
+        $row['updated']
+    );
+    $ret = $db2->query($sql);
+    if ($ret === false) {
+        echo $row["id"] . "..";
+        $errors[] = $sql;
+    }
+}
+
+echo join(PHP_EOL, $errors);
