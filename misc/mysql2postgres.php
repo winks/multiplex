64c793af84fe14ec6ae8a4fafc563f74ec958e9c
Florian Anderiasch <fa@art-core.org>
2014-02-18 22:48:47 +0100

    Finalize migration script

--- misc/mysql2postgres.php
+++ misc/mysql2postgres.php
@@ -2,23 +2,21 @@
 $user = "multiplex";
 $pass = "multiplex";
 
-$db1 = new PDO("mysql:host=localhost;dbname=simplex", $user, $pass);
+$db1 = new PDO("mysql:host=localhost;dbname=multiplex", $user, $pass);
 $db1->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 $user = "multiplex";
 $pass = "multiplex";
 
-$db2 = new PDO("pgsql:host=localhost;port=15432;dbname=multiplex", $user, $pass);
+$db2 = new PDO("pgsql:host=localhost;port=5432;dbname=multiplex", $user, $pass);
 #################################################################################
-
 $sql = "SELECT * FROM clj ORDER BY id ASC";
-
 $q1 = $db1->query($sql);
-
 $errors = array();
 
 while ($row = $q1->fetch()) {
-    $sql = "INSERT INTO clj (id, author, itemtype, url, txt, meta, tag, created, updated) VALUES('%s','%s','%s','%s','%s','%s','%s','%s','%s');";
+    $sql = "INSERT INTO clj (id, author, itemtype, url, txt, meta, tag, created, updated)".
+           " VALUES('%s','%s','%s','%s','%s','%s','%s','%s','%s');";
     $sql = sprintf(
         $sql,
         $row['id'],
@@ -33,9 +31,38 @@ while ($row = $q1->fetch()) {
     );
     $ret = $db2->query($sql);
     if ($ret === false) {
-        echo $row["id"] . "..";
+        echo $row["id"] . " ";
         $errors[] = $sql;
     }
 }
 
-echo join(PHP_EOL, $errors);
+echo "Post errors: ".join(PHP_EOL, $errors).PHP_EOL;
+#################################################################################
+$sql = "SELECT * FROM users ORDER BY uid ASC";
+$q1 = $db1->query($sql);
+$errors = array();
+
+while ($row = $q1->fetch()) {
+    $sql = "INSERT INTO users(uid,username,email,password,apikey,signupcode,created,updated)".
+           " VALUES('%s','%s','%s','%s','%s','%s','%s','%s')";
+    $sql = sprintf(
+        $sql,
+        $row['uid'],
+        $row['username'],
+        $row['email'],
+        $row['password'],
+        $row['apikey'],
+        $row['signupcode'],
+        $row['created'],
+        $row['updated']
+    );
+
+    $ret = $db2->query($sql);
+    if ($ret === false) {
+        echo $row["uid"] . " ";
+        $errors[] = $sql;
+    }
+}
+
+echo "User errors: ".join(PHP_EOL, $errors).PHP_EOL;
+#################################################################################
