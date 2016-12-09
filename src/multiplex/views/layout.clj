e5f7cfe81479dd102a71be4d5a3b300b1d06eb37
Florian Anderiasch <fa@art-core.org>
2016-12-09 10:42:56 +0000

    Fix NPE in type-pagination

--- src/multiplex/views/layout.clj
+++ src/multiplex/views/layout.clj
@@ -15,26 +15,26 @@
 
 (parser/add-tag! :csrf-field (fn [_ _] (anti-forgery-field)))
 
+(defn phelper [type params]
+  (let [p (util/int-or-default (:page params) 1)
+        l (util/int-or-default (:limit params) config/default-limit)]
+    (util/type-pagination type p l)))
+
 (defn render [template & [params]]
   (let [uid (util/int-or-default (:uid (:post params)) 0)
         theme (if (> uid 0)(:theme (nth config/user-data uid)) (:theme config-fallback))
         cfg (assoc (or config/multiplex config-fallback) :theme theme)
         user-link (if-let [host (util/is-custom-host)] host (:page-url config/multiplex))
-        type-navi-link (util/type-pagination "link" (:page params) (:limit params))
-        type-navi-text (util/type-pagination "text" (:page params) (:limit params))
-        type-navi-image (util/type-pagination "image" (:page params) (:limit params))
-        type-navi-audio (util/type-pagination "audio" (:page params) (:limit params))
-        type-navi-video (util/type-pagination "video" (:page params) (:limit params))
         page-title (if-let [x (:title (:post params))] x (:page-title cfg))
         page-header (if-let [x (:title (:post params))] x (str (:username (:post params)) "'s multiplex" ))]
     (parser/render-file (str template-path template)
                         (assoc params :context (:context *request*)
                                       :page-header page-header
                                       :page-title page-title
-                                      :type-navi-link type-navi-link
-                                      :type-navi-text type-navi-text
-                                      :type-navi-image type-navi-image
-                                      :type-navi-audio type-navi-audio
-                                      :type-navi-video type-navi-video
+                                      :type-navi-link (phelper "link" params)
+                                      :type-navi-text (phelper "text" params)
+                                      :type-navi-image (phelper "image" params)
+                                      :type-navi-audio (phelper "audio" params)
+                                      :type-navi-video (phelper "video" params)
                                       :user-link "/"
                                       :multiplex cfg))))
