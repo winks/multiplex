91e8ce7cf4ab554332fed4922714b79a87033aee
Florian Anderiasch <fa@art-core.org>
2018-05-20 10:04:43 +0000

    Bump most dependencies

--- project.clj
+++ project.clj
@@ -2,28 +2,28 @@
   :description "A little tumblelog"
   :url "https://github.com/winks/multiplex"
   :dependencies
-  [[org.clojure/clojure "1.6.0"]
-   [lib-noir "0.9.9"]
-   [compojure "1.4.0"]
-   [ring-server "0.4.0"]
-   [selmer "0.9.2"]
-   [clj-time "0.11.0"]
-   [com.taoensso/timbre "4.1.2"]
-   [com.taoensso/tower "3.0.0" :exclusions [org.clojure/clojure]]
-   [markdown-clj "0.9.47"]
-   [org.clojure/java.jdbc "0.3.7"]
-   [mysql/mysql-connector-java "5.1.31"]
-   [postgresql "9.3-1102.jdbc41"]
-   [korma "0.3.3"]
-   [digest "1.4.4"]
-   [clj-rss "0.2.3"]
+  [[org.clojure/clojure "1.9.0"]
    [org.clojure/data.json "0.2.6"]
-   [org.clojure/tools.trace "0.7.8"]
-   [log4j "1.2.15" :exclusions [javax.mail/mail
+   [org.clojure/java.jdbc "0.3.7"]
+   [org.clojure/tools.trace "0.7.9"]
+   [clj-rss "0.2.3"]
+   [clj-time "0.14.4"]
+   [compojure "1.6.1"]
+   [digest "1.4.8"]
+   [korma "0.3.3"]
+   [lib-noir "0.9.9"]
+   [markdown-clj "1.0.2"]
+   [ring-server "0.5.0"]
+   [selmer "1.11.7"]
+   [com.taoensso/timbre "4.10.0"]
+   [com.taoensso/tower "3.1.0-beta5" :exclusions [org.clojure/clojure]]
+   [mysql/mysql-connector-java "5.1.31"]
+   [org.postgresql/postgresql "42.2.2.jre7"]
+   [log4j "1.2.17" :exclusions [javax.mail/mail
                                 javax.jms/jms
                                 com.sun.jdmk/jmxtools
                                 com.sun.jmx/jmxri]]]
-  :plugins [[lein-ring "0.9.7"]]
+  :plugins [[lein-ring "0.12.4"]]
   :ring {:handler multiplex.handler/app,
          :init    multiplex.handler/init,
          :destroy multiplex.handler/destroy}
@@ -32,9 +32,9 @@
    :production {:ring {:open-browser? false
                        :stacktraces?  false
                        :auto-reload?  false}}
-   :dev {:dependencies [[ring/ring-mock "0.3.0"]
-                        [ring/ring-core "1.4.0"]
-                        [pjstadig/humane-test-output "0.7.0"]]
+   :dev {:dependencies [[ring/ring-mock "0.3.2"]
+                        [ring/ring-core "1.6.3"]
+                        [pjstadig/humane-test-output "0.8.3"]]
          :env {:dev true}
          :ring {:open-browser? false}}}
-  :min-lein-version "2.0.0")
+  :min-lein-version "2.7.0")
