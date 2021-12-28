0238e9d1a4fbb37ee21e38f530d0f4df9ff50951
Florian Anderiasch <fa@art-core.org>
2021-12-29 00:03:51 +0100

    display other post types

--- src/clj/multiplex/layout.clj
+++ src/clj/multiplex/layout.clj
@@ -1,5 +1,7 @@
 (ns multiplex.layout
   (:require
+    [multiplex.config :as config]
+    [multiplex.util :as util]
     [clojure.java.io]
     [selmer.parser :as parser]
     [selmer.filters :as filters]
@@ -13,6 +15,13 @@
 (parser/add-tag! :csrf-field (fn [_ _] (anti-forgery-field)))
 (filters/add-filter! :markdown (fn [content] [:safe (md-to-html-string content)]))
 
+(def config-fallback {:page-title "multiplex"
+                      :page-url ""
+                      :page-scheme "http"
+                      :assets-scheme "http"
+                      :bookmark-text "Bookmark me"
+                      :theme "default"})
+
 (defn render
   "renders the HTML template located relative to resources/html"
   [request template & [params]]
@@ -24,6 +33,28 @@
           :page template
           :csrf-token *anti-forgery-token*)))
     "text/html; charset=utf-8"))
+
+(defn render2
+  "renders the HTML template located relative to resources/html"
+  [request template & [params]]
+  (let [theme (first (filter seq [(:theme (:post params)) (:theme (config/env :multiplex)) (:theme config-fallback)]))
+        cfg           (assoc (first (filter seq [(config/env :multiplex) config-fallback])) :theme theme)
+        assets-prefix (if-let [site (:assets-url cfg)] (util/make-url (:assets-scheme cfg) site) "")
+        page-title    (if-let [x (:title (:post params))] x (:page-title cfg))
+        page-header   (if-let [x (:title (:post params))] x (str (:username (:post params)) "'s multiplex" ))]
+  (content-type
+    (ok
+      (parser/render-file
+        template
+        (assoc params
+          :page-header page-header
+          :page-title page-title
+          :theme theme
+          :assets-prefix assets-prefix
+          :base-url (util/make-url (:page-scheme cfg) (:page-url cfg))
+          :page template
+          :csrf-token *anti-forgery-token*)))
+    "text/html; charset=utf-8")))
 
 (defn error-page
   "error-details should be a map containing the following keys:
