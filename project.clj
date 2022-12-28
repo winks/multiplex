6cf4d2d3493486cea2110adef0113b24fd97e292
Florian Anderiasch <fa@art-core.org>
2022-12-28 17:37:01 +0100

    Bump deps, release 2.0.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject multiplex "2.0.6"
+(defproject multiplex "2.0.7"
 
   :description "multiplex - a small tumblelog"
   :url "http://github.com/winks/multiplex"
@@ -8,22 +8,22 @@
                  [buddy/buddy-hashers "1.8.158"]
                  [buddy/buddy-sign "3.4.333"]
                  [ch.qos.logback/logback-classic "1.2.11"]
-                 [clojure.java-time "0.3.3"]
-                 [conman "0.9.4"]
+                 [clojure.java-time "1.1.0"]
+                 [conman "0.9.5"]
                  [cprop "0.1.19"]
                  [expound "0.9.0"]
                  [funcool/struct "1.4.0"]
                  [json-html "0.4.7"]
-                 [luminus-migrations "0.7.1"]
+                 [luminus-migrations "0.7.5"]
                  [luminus-transit "0.1.5"]
-                 [luminus-undertow "0.1.14"]
+                 [luminus-undertow "0.1.16"]
                  [luminus/ring-ttl-session "0.3.3"]
-                 [markdown-clj "1.11.1"]
+                 [markdown-clj "1.11.4"]
                  [metosin/muuntaja "0.6.8"]
-                 [metosin/reitit "0.5.15"]
+                 [metosin/reitit "0.5.18"]
                  [metosin/ring-http-response "0.9.3"]
-                 [mount "0.1.16"]
-                 [nrepl "0.9.0"]
+                 [mount "0.1.17"]
+                 [nrepl "1.0.0"]
                  [org.clojure/clojure "1.11.1"]
                  [org.clojure/data.json "2.4.0"]
                  [org.clojure/tools.cli "1.0.206"]
@@ -34,10 +34,10 @@
                  [org.webjars/webjars-locator "0.45"]
                  [org.webjars/webjars-locator-jboss-vfs "0.1.0"]
                  [ring-webjars "0.2.0"]
-                 [ring/ring-core "1.9.5"]
-                 [ring/ring-defaults "0.3.3"]
+                 [ring/ring-core "1.9.6"]
+                 [ring/ring-defaults "0.3.4"]
                  [ring/ring-ssl "0.3.0"]
-                 [selmer "1.12.50"]]
+                 [selmer "1.12.55"]]
 
   :min-lein-version "2.0.0"
 
